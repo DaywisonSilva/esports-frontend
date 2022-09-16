4ad7fce0fab779705467ba11f1e5b81728f6a6da
DaywisonSilva <daywisonsilvasou@gmail.com>
2022-09-16 00:10:20 -0300

    feat: to handle with cards

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,56 @@
     <h1 class="main__title">
       Seu <span class="main__title--emphasis">duo</span> está aqui
     </h1>
+    <div class="main__listCards">
+      <a href="" class="card">
+        <img src="/img/image1.png" alt="image1" />
+        <div class="card__info">
+          <strong class="card__title">League of Legends</strong>
+          <br />
+          <span class="card__subtitle">4 anúncios</span>
+        </div>
+      </a>
+      <a href="" class="card">
+        <img src="/img/image2.png" alt="image 2" />
+        <div class="card__info">
+          <strong class="card__title">Dota 2</strong>
+          <br />
+          <span class="card__subtitle">4 anúncios</span>
+        </div>
+      </a>
+      <a href="" class="card">
+        <img src="/img/image3.png" alt="image 3" />
+        <div class="card__info">
+          <strong class="card__title">Counter Strike</strong>
+          <br />
+          <span class="card__subtitle">4 anúncios</span>
+        </div>
+      </a>
+      <a href="" class="card">
+        <img src="/img/image4.png" alt="image 4" />
+        <div class="card__info">
+          <strong class="card__title">Apex Legends</strong>
+          <br />
+          <span class="card__subtitle">4 anúncios</span>
+        </div>
+      </a>
+      <a href="" class="card">
+        <img src="/img/image5.png" alt="image 5" />
+        <div class="card__info">
+          <strong class="card__title">Fortnite</strong>
+          <br />
+          <span class="card__subtitle">4 anúncios</span>
+        </div>
+      </a>
+      <a href="" class="card">
+        <img src="/img/image6.png" alt="image 6" />
+        <div class="card__info">
+          <strong class="card__title">World of Warcraft</strong>
+          <br />
+          <span class="card__subtitle">4 anúncios</span>
+        </div>
+      </a>
+    </div>
   </main>
 </template>
 
@@ -15,11 +65,31 @@
   @apply max-w-[1344px] mx-auto flex justify-center items-center flex-col pt-20;
 
   .main__title {
-    @apply text-6xl text-white font-black mt-20;
+    @apply text-6xl  font-black mt-20;
   }
 
   .main__title--emphasis {
     @apply bg-nlw-gradient bg-clip-text text-transparent;
+  }
+
+  .main__listCards {
+    @apply grid grid-cols-6 gap-6 mt-16;
+  }
+
+  .card {
+    @apply relative rounded-lg overflow-hidden;
+  }
+
+  .card__info {
+    @apply w-full pt-16 pb-4 px-4 absolute bottom-0 left-0 bg-game-gradient;
+  }
+
+  .card__title {
+    @apply font-bold;
+  }
+
+  .card__subtitle {
+    @apply text-zinc-300 text-sm;
   }
 }
 </style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,8 @@ module.exports = {
     extend: {
       colors: {},
       backgroundImage: {
+        'game-gradient':
+          'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
         'nlw-gradient':
           'linear-gradient(89.86deg, #9572FC 10.08%, #43E7AD 53.94%, #E1D55D 100%)',
         galaxy: "url('./img/background-galaxy.png')"
